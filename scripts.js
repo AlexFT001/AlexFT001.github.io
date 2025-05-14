d6ee244458a7633ffc55218a648c314c68ff494d
AlexFT001 <torres.alex.at04@gmail.com>
2025-05-14 23:48:43 +0100

    Ta bom?
    Como eu sou o cliente SIM :)

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const repoList = document.getElementById('repo-list');
+const username = "alexft001";
+const portgolioLink = `${username}.github.io`;
+
+
+fetch(`https://api.github.com/users/${username}/repos`)
+    .then(response => response.json())
+    .then(repositories => {
+        repositories.forEach(repo => {
+            if((repo.name).toLowerCase() != portgolioLink.toLowerCase()) {
+                const repoDiv = document.createElement('div')
+                repoDiv.className = 'repo';
+            
+                const link = document.createElement('a');
+                link.textContent = repo.name;
+                link.href = `https://${portgolioLink}/${repo.name}/`;
+                link.target = '_blank';
+            
+                repoDiv.appendChild(link);
+                repoList.appendChild(repoDiv);    
+            };
+        });
+    })
+    .catch(error => {
+        console.error('Error a buscar os Repositorios', error);
+        repoList.innerHTML = '<p>Failed to load repositories.<p>'
+    });
